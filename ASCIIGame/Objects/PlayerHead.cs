6b2b8dba30e59aca7fc64ef30ba91df3dbe561c3
Danila Pokhabov <danilapokhabov@gmail.com>
2020-05-20 02:14:28 +0700

    Renamed Player to PlayerHead and added PlayerTail

--- ASCIIGame/Objects/PlayerHead.cs
+++ ASCIIGame/Objects/PlayerHead.cs
@@ -8,13 +8,19 @@ using ASCIIEngine.Core.BasicClasses;
 
 namespace ASCIIGame.Objects
 {
-    public class Player : GameObject
+    public class PlayerHead : GameObject
     {
         public override bool HasCollider => true;
+        public override string Tag => "player";
+        public override int Layer => 4;
 
         private Vector2D _prevPos;
 
         private GameManager gm;
+
+        private Vector2D _direction;
+
+        public PlayerTail Tail;
 
         public override void OnCollision(IEnumerable<GameObject> collidedWith)
         {
@@ -41,27 +47,48 @@ namespace ASCIIGame.Objects
             this.Material = new Material
             {
                 Character = '@',
-                ForegroundColor = Color.DarkCyan
+                ForegroundColor = Color.IndianRed
             };
         }
 
         public override void Step()
         {
             _prevPos = this.Position;
+
             switch (Input.ActiveKey)
             {
                 case ConsoleKey.UpArrow:
-                    this.Position += Vector2D.Up;
+                    _direction = Vector2D.Up;
                     break;
                 case ConsoleKey.DownArrow:
-                    this.Position += Vector2D.Down;
+                    _direction = Vector2D.Down;
                     break;
                 case ConsoleKey.LeftArrow:
-                    this.Position += Vector2D.Left;
+                    _direction = Vector2D.Left;
                     break;
                 case ConsoleKey.RightArrow:
-                    this.Position += Vector2D.Right;
+                    _direction = Vector2D.Right;
                     break;
+            }
+
+            this.Position += _direction;
+            if (Tail != null) Tail.Position = _prevPos;
+        }
+
+        public void AddTail()
+        {
+            if (Tail == null)
+            {
+                var newTail = new PlayerTail
+                {
+                    Position = _prevPos
+                };
+                GameObjectPoolSingleton.Instance.AddObject(newTail);
+                Tail = newTail;
+            }
+            else
+            {
+                Tail.AddTail();
             }
         }
     }
---
+++ ASCIIGame/Objects/PlayerTail.cs
@@ -0,0 +1,48 @@
+﻿using System.Drawing;
+using ASCIIEngine.Core;
+using ASCIIEngine.Core.BasicClasses;
+
+namespace ASCIIGame.Objects
+{
+    public class PlayerTail : GameObject
+    {
+        public PlayerTail Next { get; set; }
+        public PlayerTail Tail => Next?.Tail ?? Next ?? this;
+
+        private Vector2D _prevPos;
+
+        public override int Layer => 3;
+
+        public override void Start()
+        {
+            this.Material = new Material
+            {
+                Character = '@',
+                ForegroundColor = Color.DarkCyan
+            };
+        }
+
+        public override void Step()
+        {
+            if (Next != null) Next.Position = _prevPos;
+            _prevPos = this.Position;
+        }
+
+        public void AddTail()
+        {
+            if (Tail == this)
+            {
+                var newTail = new PlayerTail
+                {
+                    Position = _prevPos
+                };
+                GameObjectPoolSingleton.Instance.AddObject(newTail);
+                this.Next = newTail;
+            }
+            else
+            {
+                Tail.AddTail();
+            }
+        }
+    }
+}
