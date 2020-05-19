7833ef50aecce34490b878162df499ac9b3216b5
Vlad <vladskryto@gmail.com>
2020-05-20 00:50:29 +0400

    Some unit-tests

---
+++ ASCIIEngine.UnitTests/BasicClasses/MaterialTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Drawing;
+using ASCIIEngine.Core.BasicClasses;
+using FluentAssertions;
+using Xunit;
+
+namespace ASCIIEngine.UnitTests.BasicClasses
+{
+    public class MaterialTests
+    {
+        [Fact]
+        public void EqualsOperatorWithEqualMaterial_ShouldReturnTrue()
+        {
+            var firstMaterial = new Material('@', Color.Aquamarine, Color.Chocolate);
+            var secondMaterial = new Material('@', Color.Aquamarine, Color.Chocolate);
+            (firstMaterial == secondMaterial).Should().BeTrue();
+        }
+        
+        [Fact]
+        public void EqualsOperatorWithNotEqualMaterials_ShouldReturnFalse()
+        {
+            var firstMaterial = new Material('@', Color.Aquamarine, Color.Chocolate);
+            var secondMaterial = new Material('=', Color.Aquamarine, Color.Chocolate);
+            var thirdMaterial = new Material('@', Color.Chocolate, Color.Aquamarine);
+            var fourthMaterial = new Material('@', Color.Aquamarine, Color.Coral); 
+            var fifthMaterial = new Material('@', Color.Aqua, Color.Chocolate);
+            (firstMaterial == secondMaterial).Should().BeFalse();
+            (firstMaterial == thirdMaterial).Should().BeFalse();
+            (firstMaterial == fourthMaterial).Should().BeFalse();
+            (firstMaterial == fifthMaterial).Should().BeFalse();
+        }
+        
+        [Fact]
+        public void NotEqualsOperatorWithEqualMaterial_ShouldReturnTrue()
+        {
+            var firstMaterial = new Material('@', Color.Aquamarine, Color.Chocolate);
+            var secondMaterial = new Material('@', Color.Aquamarine, Color.Chocolate);
+            (firstMaterial != secondMaterial).Should().BeFalse();
+        }
+        
+        [Fact]
+        public void NotEqualsOperatorWithNotEqualMaterials_ShouldReturnFalse()
+        {
+            var firstMaterial = new Material('@', Color.Aquamarine, Color.Chocolate);
+            var secondMaterial = new Material('=', Color.Aquamarine, Color.Chocolate);
+            var thirdMaterial = new Material('@', Color.Chocolate, Color.Aquamarine);
+            var fourthMaterial = new Material('@', Color.Aquamarine, Color.Coral); 
+            var fifthMaterial = new Material('@', Color.Aqua, Color.Chocolate);
+            (firstMaterial != secondMaterial).Should().BeTrue();
+            (firstMaterial != thirdMaterial).Should().BeTrue();
+            (firstMaterial != fourthMaterial).Should().BeTrue();
+            (firstMaterial != fifthMaterial).Should().BeTrue();
+        }
+    }
+}
---
+++ ASCIIEngine.UnitTests/RendererTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Drawing;
+using ASCIIEngine.Core;
+using ASCIIEngine.Core.BasicClasses;
+using FluentAssertions;
+using Xunit;
+
+namespace ASCIIEngine.UnitTests
+{
+    public class RendererTests
+    {
+        [Fact]
+        public void Render_ShouldReturnRenderedOutput()
+        {
+            var baseCore = new Base();
+            baseCore.AddObject(new GameObject
+            {
+                Layer = 1,
+                Material = new Material('@', Color.Red, Color.Blue),
+                Position = new Vector2D(2, 2),
+                Tag = "test_object",
+                HasCollider = true
+            });
+            
+            var renderer = new Renderer(new Vector2D(5, 5));
+            
+            var buffer = new Material[renderer.Size.X, renderer.Size.Y];
+            var output = renderer.Render(buffer);
+
+            var testObject = output[2, 2];
+            testObject.Character.Should().Be('@');
+            testObject.ForegroundColor.Should().Be(Color.Red);
+            testObject.BackgroundColor.Should().Be(Color.Blue);
+            
+            baseCore.AddObject(new GameObject
+            {
+                Layer = 2,
+                Material = new Material('#', Color.Blue, Color.Red),
+                Position = new Vector2D(2, 2),
+                Tag = "test_object_2",
+                HasCollider = true
+            });
+            
+            var newOutput = renderer.Render(buffer);
+            var secondTestObject = newOutput[2, 2];
+            secondTestObject.Character.Should().Be('#');
+            secondTestObject.ForegroundColor.Should().Be(Color.Blue);
+            secondTestObject.BackgroundColor.Should().Be(Color.Red);
+        }
+    }
+}
