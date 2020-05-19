455b3952b0587fb680a097b3a309e30f74a0db43
Danila Pokhabov <danilapokhabov@gmail.com>
2020-05-20 02:15:39 +0700

    Added grass animations

--- ASCIIGame/Objects/Grass.cs
+++ ASCIIGame/Objects/Grass.cs
@@ -10,15 +10,25 @@ namespace ASCIIGame.Objects
     class Grass : GameObject
     {
         public override bool HasCollider => false;
+        private Random rand;
         public override int Layer => 1;
 
         public override void Start()
         {
+            rand = new Random();
             this.Material = new Material()
             {
                 Character = '·',
                 ForegroundColor = Color.DarkGreen
             };
+        }
+
+        public override void Step()
+        {
+            var mat = this.Material;
+            mat.Character = rand.Next(0, 2) == 0 ? '·' : ',';
+            mat.ForegroundColor = rand.Next(0, 2) == 0 ? Color.ForestGreen : Color.DarkGreen;
+            this.Material = mat;
         }
     }
 }
