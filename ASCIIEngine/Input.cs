05157f763503bc44986c4605b019adc88a7524a0
Danila Pokhabov <danilapokhabov@gmail.com>
2020-05-22 19:41:02 +0700

    Added ActiveDirection

--- ASCIIEngine/Input.cs
+++ ASCIIEngine/Input.cs
@@ -1,4 +1,5 @@
 using System;
+using ASCIIEngine.Core.BasicClasses;
 
 namespace ASCIIEngine.Core
 {
@@ -6,9 +7,27 @@ namespace ASCIIEngine.Core
     {
         public static ConsoleKey ActiveKey { get; private set; }
 
+        public static Vector2D ActiveDirection =>
+            GameInput ? ActiveKey switch
+            {
+                ConsoleKey.UpArrow => Vector2D.Up,
+                ConsoleKey.DownArrow => Vector2D.Down,
+                ConsoleKey.LeftArrow => Vector2D.Left,
+                ConsoleKey.RightArrow => Vector2D.Right,
+                _ => new Vector2D()
+            } : new Vector2D();
+
+        public static bool GameInput { get; private set; }
+
         public static void SetPressedKey(ConsoleKey key)
         {
             ActiveKey = key;
+
+            // ` key
+            if (key == ConsoleKey.Oem3)
+            {
+                GameInput = !GameInput;
+            }
         }
     }
 }
